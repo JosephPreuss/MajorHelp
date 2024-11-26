013761080244077b9aef193ad8f10ab28812d90f
Druv Patel <druv@email.sc.edu>
2024-11-26 12:25:20 -0500

    removed reviews for major and Uni from database

--- MajorHelp/apps.py
+++ MajorHelp/apps.py
@@ -5,7 +5,3 @@ class MajorhelpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MajorHelp'
 
-    # clears all reviews on every server start
-    def ready(self):
-        from MajorHelp.models import MajorReview
-        MajorReview.objects.all().delete()
