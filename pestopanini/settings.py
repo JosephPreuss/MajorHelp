4eadc0424b400aaad635adf7266986ffe972b935
Brandon Riley <Boriley@email.sc.edu>
2024-12-05 17:50:31 -0500

    Fixed Main branch

--- pestopanini/settings.py
+++ pestopanini/settings.py
@@ -77,11 +77,10 @@ WSGI_APPLICATION = 'pestopanini.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
