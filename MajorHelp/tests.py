d475e5e4076db68e61ee5ef6d38152ebcde770c7
Jpr3uss <jpreuss@email.sc.edu>
2025-01-17 20:58:51 -0500

    Added 3 unit tests for the Calc.

--- MajorHelp/tests.py
+++ MajorHelp/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
 
+from django.test import Client
+
+from django.urls import reverse
+
+
+
 # Create your tests here.
+
+from .models import University, Major
+class CalcTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        exampleUni = University.objects.create(name="exampleUni")
+        Major.objects.create(major_name="exampleMajor", slug="exampleMajor", university=exampleUni)
+
+
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.url = "MajorHelp:calc"
+
+
+    def testCalcReturnsHtmlPageAfterNoGetRequest(self):
+        """
+        The calculator should only return a json file if a get request is fully
+        filled out to the server, otherwise it will return an html page.
+        """
+
+        response = self.client.get(reverse(self.url))
+        
+        # check status code
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response['content-type'], 'text/html; charset=utf-8')
+
+    def testCalcReturnsHtmlPageAfterPartialGetRequest(self):
+        """
+        "Partial" is defined here as some get entries being null. While some
+        entries are already predefined with defaults, most are not.
+        """
+
+        getData = "?uni=exampleUni"
+
+        response = self.client.get(reverse(self.url)+getData)
+
+        # check status code
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response['content-type'], 'text/html; charset=utf-8')
+
+    def testCalcReturnsHtmlPageAfterFullGetRequest(self):
+        """
+        "Full" is defined here as all entries being non-null.
+
+        Note that empty entries are still considered valid.
+        """
+        
+        getData = "?uni=exampleUni&outstate=true&dept=exampleDept&major=exampleMajor&aid="
+
+        response = self.client.get(reverse(self.url)+getData)
+
+        # check status code
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response['content-type'], 'application/json')
+        
