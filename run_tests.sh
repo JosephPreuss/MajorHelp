4593f967212c04c4540752efba01b8cf89aae8ab
JPr3uss <jpreuss@email.sc.edu>
2025-04-06 14:27:58 -0400

    Updated script to now accept an argument specifying where tests should be found

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,15 @@
 
 # Function to print help statement
 help() {
-    printf "Usage $0 [OPTION]\nA helper script for running the unit and behavioral tests for MajorHelp.\nTests will be run in a test database, it and any cache can be cleaned with $0 --clean.\n\nOptional Flags:\n\t-c, --clean\t\tCleans up pycache and test database.\n\t-r, --run-test-server\tRuns the server with the test database, without any testing.\n\t-h, --help\t\tDisplays this message.\n\nScript and testing by Joseph Preuss.\n"
+    printf "Usage $0 [OPTION] [PATH]\nA helper script for running the unit and behavioral tests for MajorHelp.\n"
+    printf "Tests will be run in a test database, it and any cache can be cleaned with $0 --clean.\n"
+    printf "\nOptional Flags:\n\t-c, --clean\t\tCleans up pycache and test database.\n"
+    printf "\t-r, --run-test-server\tRuns the server with the test database, without any testing.\n"
+    printf "\t-h, --help\t\tDisplays this message.\n"
+    printf "\nOptional Positional Argument:\n"
+    printf "\t[PATH]\t Specify where to look for tests (default: current directory).\n"
+    
+    printf "\nScript and testing by Joseph Preuss.\n"
 }
 
 
@@ -54,28 +62,55 @@ activate_venv() {
     fi
 }
 
-# Check for the --clean option
-if [ "$1" == "-c" ] || [ "$1" == "--clean" ]; then
-    clean_directory
-    exit 0
-fi
+# Parse options using get opt
+TEMP=$(getopt -o crh --long clean,run-test-server,help -n "$0" -- "$@")
 
-# Check for the run server option
-if [ "$1" == "-r" ] || [ "$1" == "--run-test-server" ]; then
-    run_test_server
-    exit 0
-fi
-
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    help
-    exit 0
-fi
-
-if [ "$1" != '' ]; then
-    echo -e "Unrecognized option, $1.\n\n"
-    help
+# check if getopt was successful
+if [ $? != 0 ]; then
+    echo "Error parsing options." >&2
     exit 1
 fi
+
+# Reorder arguments as parsed by getopt
+eval set -- "$TEMP"
+
+# Process
+while true; do
+    case $1 in
+        -c|--clean)
+            clean_directory
+            exit 0
+            ;;
+        
+        -r|--run-test-server)
+
+            run_test_server
+            exit 0
+            ;;
+
+        -h|--help)
+            help
+            exit 0
+            ;;
+        
+        --)
+            shift
+            break
+            ;;
+
+        *)
+            echo -e "Unrecognized option, -$1.\n\n" >&2
+            help
+            exit 1
+            ;;
+    esac
+done
+
+
+# Default Behavior, run tests.
+
+# Shift processed options
+shift $((OPTIND - 1))
 
 # Activate the virtual environment
 activate_venv
@@ -85,16 +120,18 @@ export DJANGO_TEST_ENV=true
 
 # Set up the test database
 echo "Applying migrations to set up the test database..."
-python manage.py migrate --settings=pestopanini.test_settings
+python manage.py migrate --settings=pestopanini.test_settings &&
 
 # Start the server in the background, suppressing output
-echo "Starting the server in the background..."
+echo "Starting the server in the background..." &&
 python manage.py runserver --settings=pestopanini.test_settings &> /dev/null &
 SERVER_PID=$!
 
 # Run the tests
 echo "Running tests..."
-pytest
+
+# if $1 is given, then it will run pytest at that directory
+pytest $1
 
 # Kill the server process
 echo "Stopping the server..."
