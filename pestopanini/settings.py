05db21583840998123e5566ab8ed6326d752ba87
Chase Bradley <bradleycha@proton.me>
2025-11-11 18:28:22 -0500

    Update Django DATABASES in settings.py to use PostgreSQL

--- pestopanini/settings.py
+++ pestopanini/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'pestopanini.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'majorhelp',
+        'USER': 'postgres',
+        'PASSWORD': 'donthackme',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
