9a732475a438a4aaa02cb1d65a339d718322c294
Joseph Preuss <silverwolf901jp@gmail.com>
2025-11-18 22:51:28 -0500

    adjustments for postgres server

--- pestopanini/settings.py
+++ pestopanini/settings.py
@@ -84,8 +84,17 @@ WSGI_APPLICATION = 'pestopanini.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # For default db.sqlite3
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+
+        # For PostgreSQL
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME" : "majorhelp_db",
+        "USER" : "majorhelp_user",
+        "PASSWORD" : "password",
+        "HOST" : "localhost",
+        "PORT" : "5432",
     }
 }
 
